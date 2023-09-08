62150199a1d9a301b51b3e2a43c2b48b19841b03
SethminiDissanayake <sethminidissa@gmail.com>
2023-09-08 21:32:20 +0530

    UserDAO Created

--- src/lk/ijse/hms/dao/custom/ReservationDAO.java
+++ src/lk/ijse/hms/dao/custom/ReservationDAO.java
@@ -1,8 +1,3 @@
-/*
- * Kasun Miuranga
- * Copyright (c) 2023
- */
-
 package lk.ijse.hms.dao.custom;
 
 import lk.ijse.hms.dao.CrudDAO;
---
+++ src/lk/ijse/hms/dao/custom/UserDAO.java
@@ -0,0 +1,12 @@
+package lk.ijse.hms.dao.custom;
+
+import lk.ijse.hms.dao.CrudDAO;
+import lk.ijse.hms.entity.User;
+
+public interface UserDAO extends CrudDAO<User> {
+    String getUser(String id);
+
+    String getPassword(String id);
+
+    boolean updateUser_Pw(String newUserName, String newPw);
+}
