8c90c2e1920f701c54b0025034b6f5e4dd3db745
SethminiDissanayake <sethminidissa@gmail.com>
2023-09-08 20:28:50 +0530

    StudentDTO class  created

--- src/lk/ijse/hms/AppInitializer.java
+++ src/lk/ijse/hms/AppInitializer.java
@@ -1,5 +1,19 @@
 package lk.ijse.hms;
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/resources/view/Reservation.fxml"))));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+        primaryStage.setY(0);
+        primaryStage.setTitle("24D Hostel Management System | Login");
+    }
 }
