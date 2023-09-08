77a1498be7f047e1bc9790ea7274563cf7333d51
SethminiDissanayake <sethminidissa@gmail.com>
2023-09-08 15:07:14 +0530

    Login FXML created

---
+++ src/lk/ijse/hms/controller/LoginController.java
@@ -0,0 +1,46 @@
+package lk.ijse.hms.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.AnchorPane;
+
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class LoginController {
+
+    @FXML
+    private ImageView imgPasswordView;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private Label shownPassword;
+
+    @FXML
+    private ToggleButton toggleButton;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void forgotClickOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginClickOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void passwordFieldKeyTyped(ActionEvent event) {
+
+    }
+
+}
