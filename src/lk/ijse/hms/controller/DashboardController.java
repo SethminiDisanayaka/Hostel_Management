c11b1504ff0fdbf6a863cea139dbfbaa2933bfbf
SethminiDissanayake <sethminidissa@gmail.com>
2023-09-08 15:27:04 +0530

    Dasboard FXML Created

---
+++ src/lk/ijse/hms/controller/DashboardController.java
@@ -0,0 +1,33 @@
+package lk.ijse.hms.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DashboardController {
+
+    @FXML
+    void btnBackOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReservationClickOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRoomsClickOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSettingsClickOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudentClickOnAction(ActionEvent event) {
+
+    }
+
+}
