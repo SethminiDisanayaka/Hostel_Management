33b8b1d3d2bfbd8d175e4d648cf1f809fd300875
SethminiDissanayake <sethminidissa@gmail.com>
2023-09-08 23:26:14 +0530

    UserBOImpl Created

--- src/lk/ijse/hms/bo/custom/UserBO.java
+++ src/lk/ijse/hms/bo/custom/UserBO.java
@@ -1,4 +1,11 @@
 package lk.ijse.hms.bo.custom;
 
-public interface UserBO {
+import lk.ijse.hms.bo.SuperBO;
+
+public interface UserBO extends SuperBO {
+    String getUser(String id);
+
+    String getPassword(String id);
+
+    boolean updateUser_Pw(String newUserName, String newPw);
 }
